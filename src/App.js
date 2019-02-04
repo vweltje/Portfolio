fc4bb40f7a90305b06625b9fce8946799ba88826
Vincent Weltje <vincentweltje@gmail.com>
2019-02-04 20:56:58 +1000

    rename netlify folder and added in react helmet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Helmet } from 'react-helmet'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import About from './pages/About'
 import Work from './pages/Work'
@@ -12,6 +13,23 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="App">
+          <Helmet>
+            <meta charSet="utf-8" />
+            <meta
+              http-equiv="Content-Type"
+              content="text/html; charset=UTF-8"
+            />
+            <title>Portfolio | Vincent Weltje</title>
+            <meta
+              name="description"
+              content="Vincent Weltje | Portfolio and travel blog"
+            />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0"
+            />
+          </Helmet>
+
           <Switch>
             <Route exact path="/" component={About} />
             <Route exact path="/work" component={Work} />
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
