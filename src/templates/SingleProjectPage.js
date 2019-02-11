379c3b35dfa4750a967e8cd0e2fcdd071b611221
Vincent Weltje <vincentweltje@gmail.com>
2019-02-11 20:07:00 +1000

    Added Single Project page template file

---
+++ src/templates/SingleProjectPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+import './SingleProjectPage.css'
+
+const SingleProjectPage = () => (
+  <Layout>
+    <main className="SingleProject">
+      <h2 className="Home--Heading">SingleProjectPage</h2>
+      <div className="Backgound--Shape" />
+    </main>
+  </Layout>
+)
+
+export default SingleProjectPage
